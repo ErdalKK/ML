6fab6af127e3ee42eac873f8ce8840ce4561d8ae
NiklasR97 <89777308+NiklasR97@users.noreply.github.com>
2021-11-26 16:39:17 +0100

    Update Data_exploration.R

--- Data_exploration.R
+++ Data_exploration.R
@@ -1,1 +1,7 @@
 # test
+
+temp <- tempfile(fileext = ".zip")
+download.file("https://drive.google.com/uc?authuser=0&id=1AiZda_1-2nwrxI8fLD0Y6e5rTg7aocv0&export=download",
+  temp)
+out <- unzip(temp, exdir = tempdir())
+bank <- read.csv(out[14], sep = ";")
